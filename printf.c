8623c1446eed3a333a9c374653de428ddb54bb5b
duplebado <ajibadeadebayoengr@gmail.com>
2021-07-27 09:50:55 +0000

    Update null check

--- printf.c
+++ printf.c
@@ -13,10 +13,10 @@ int _printf(const char *format, ...)
 	va_list args;
 	int (*fmt_func)(va_list *);
 
-	va_start(args, format);
-
 	if (!format)
 		return (-1);
+
+	va_start(args, format);
 
 	while (va_arg_loop_condition)
 	{
